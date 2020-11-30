7bd4b49c58b56f888e689793b07525de5f5b2254
Uwe Jäger <uwej711@googlemail.com>
2020-11-30 11:38:57 +0100

    remove deprecations and adjust form types

--- Twig/SiteExtension.php
+++ Twig/SiteExtension.php
@@ -6,9 +6,12 @@
  */
 namespace Valiton\Bundle\MultiSiteBundle\Twig;
 
+use Twig\Extension\AbstractExtension;
+use Twig\Extension\GlobalsInterface;
+use Twig\TwigFilter;
 use Valiton\Bundle\MultiSiteBundle\Templating\SiteHelper;
 
-class SiteExtension extends \Twig_Extension
+class SiteExtension extends AbstractExtension implements GlobalsInterface
 {
     /** @var  SiteHelper */
     protected $siteHelper;
@@ -26,23 +29,13 @@ class SiteExtension extends \Twig_Extension
     public function getFilters()
     {
         return array(
-            'base64_icon' => new \Twig_SimpleFilter('base64_icon', array($this, 'base64Icon'))
+            'base64_icon' => new TwigFilter('base64_icon', array($this, 'base64Icon'))
         );
     }
 
     public function base64Icon($icon)
     {
         return 'data:image/x-icon;base64,'.base64_encode($icon->getFileContent());
-    }
-
-    /**
-     * Returns the name of the extension.
-     *
-     * @return string The extension name
-     */
-    public function getName()
-    {
-        return 'valiton_multi_site.site_extension';
     }
 
 }
